ce9f2664b3925a45e2deddec7c09d772ffb48eb6
Ben Doherty <ben@notionparallax.co.uk>
2016-04-29 09:38:37 -0700

    mongo attempt

--- index.js
+++ index.js
@@ -18,3 +18,18 @@ app.listen(app.get('port'), function() {
 });
 
 
+var MongoClient = require('mongodb').MongoClient
+
+var URL = 'mongodb://heroku_d58bh1wm:m3f37fh3q0sodg1lct1vbb97s0@ds063899.mlab.com:63899/heroku_d58bh1wm'
+
+MongoClient.connect(URL, function(err, db) {
+  if (err) return
+
+  var collection = db.collection('foods')
+  collection.insert({name: 'taco', tasty: true}, function(err, result) {
+    collection.find({name: 'taco'}).toArray(function(err, docs) {
+      console.log(docs[0])
+      db.close()
+    })
+  })
+})
